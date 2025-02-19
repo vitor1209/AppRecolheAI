b0679a127fab9907387055d60a2c52cc72288ad6
vitor1209 <vitor.silva.lopes1209@gmail.com>
2025-02-18 21:39:13 -0300

    MapsPage, create mapa e modal

--- lib/pages/mapsPage.dart
+++ lib/pages/mapsPage.dart
@@ -3,6 +3,8 @@
 import 'package:apprecolheai/pages/homePage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class Mapspage extends StatefulWidget {
   const Mapspage({super.key});
@@ -17,18 +19,12 @@ class _MapspageState extends State<Mapspage> {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Mapa',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          _buildMaps(screenWidth, screenHeight, context),
+          SizedBox(height: screenHeight * 0.02),
+        ],
       ),
-
       bottomNavigationBar: Stack(
         alignment: Alignment.bottomCenter,
         children: [
@@ -85,4 +81,107 @@ class _MapspageState extends State<Mapspage> {
       ),
     );
   }
+}
+
+Widget _buildMaps(
+  double screenWidth,
+  double screenHeight,
+  BuildContext context,
+) {
+  return SizedBox(
+    height: screenHeight * 0.5,
+    width: screenWidth,
+    child: Stack(
+      children: [
+        FlutterMap(
+          options: MapOptions(
+            initialCenter: LatLng(-23.703947464720905, -46.41383419958605),
+            initialZoom: 17,
+          ),
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'com.example.apprecolheai',
+            ),
+            MarkerLayer(
+              markers: [
+                Marker(
+                  point: LatLng(-23.703947464720905, -46.41383419958605),
+                  child: IconButton(
+                    onPressed: () {
+                      // Chama o modal ao clicar no botão
+                      _showModal(context);
+                    },
+                    icon: Icon(Icons.location_on, color: Colors.red, size: 35),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Positioned(
+          top: screenHeight * 0.05,
+          right: screenWidth * 0.05,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color(0xFFDCE8E4),
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.gps_fixed, size: 30),
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+void _showModal(BuildContext context) {
+  double screenHeight = MediaQuery.of(context).size.height;
+  double screenWidth = MediaQuery.of(context).size.width;
+  showModalBottomSheet(
+    context: context,
+    isScrollControlled: true,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+    ),
+    builder: (BuildContext context) {
+      return Container(
+        padding: EdgeInsets.all(16.0),
+        height: screenHeight * 0.5,
+        width: screenWidth,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Informações da Lixeira',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text('Preenchimento da lixeira:'),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Image.asset(
+                  'lib/assets/images/LixeiraFalse.png',
+                  height: screenHeight * 0.1,
+                ),
+                SizedBox(width: 10),
+                Text('Vazia'),
+              ],
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('Fechar'),
+            ),
+          ],
+        ),
+      );
+    },
+  );
 }
